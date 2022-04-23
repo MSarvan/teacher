dcb721eadf9b96158f287b4c9f3fa84ea4c34c60
Saravanan <sarvan.m20@gmail.com>
2022-04-23 11:42:05 +0530

    updated

--- Frontend/teacher/src/Components/Home.jsx
+++ Frontend/teacher/src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Navbar } from "./Navbar";
+import "../App.css";
 
 export const Home = () => {
   const [showdata, setShowdata] = useState([]);
@@ -13,13 +14,13 @@ export const Home = () => {
     <div>
       <Navbar />
       {showdata.map((e) => (
-          <div key={e._id}>
-            <p>{e.img}</p>
-            <h2>{e.name}</h2>
-            <p>Age: {e.age}</p>
-            <p>Gender: {e.gender}</p>
-            {/* <p>Class: {e.class.map((e) => )}</p> */}
-          </div>
+        <div className="display" key={e._id}>
+          <p>{e.img}</p>
+          <h2>{e.name}</h2>
+          <p>Age: {e.age}</p>
+          <p>Gender: {e.gender}</p>
+          {/* <p>Class: {e.class.map((e) => )}</p> */}
+        </div>
       ))}
     </div>
   );
